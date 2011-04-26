356fb027df9d384d99e7ce1c139eb9db11179fe1
Malte S. Stretz <mss@apache.org>
2011-04-26 14:37:22 +0200

    Fixup test script.

--- readshortcut.pl
+++ readshortcut.pl
@@ -9,6 +9,13 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
-use File::Shortcut;
+use File::Shortcut qw(read_shortcut);
 
-File::Shortcut::read_shortcut(@ARGV);
+use Data::Dumper;
+
+my $data = read_shortcut(@ARGV);
+unless ($data) {
+  say $File::Shortcut::errstr;
+  exit 1;
+}
+print Dumper($data);
