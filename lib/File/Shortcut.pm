9a3264c574cc84d0b2a0316e79e26e7ce2b0a19b
Malte S. Stretz <mss@apache.org>
2011-04-28 19:24:02 +0200

    Add link to GitHub.

--- lib/File/Shortcut.pm
+++ lib/File/Shortcut.pm
@@ -142,6 +142,10 @@ You can also look for information at:
 
 =over 4
 
+=item * GitHub: Code repository
+
+L<https://github.com/mss/file-shortcut>
+
 =item * RT: CPAN's request tracker
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Shortcut>
